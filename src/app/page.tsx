5ecfd6b1c240f6e8458174ca68b57cdaecc58661
Akhil Waghmare <akhilwaghmare@gmail.com>
2024-03-10 19:04:03 -0700

    Escaped character on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { z } from "zod";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-20 p-24">
-      <h1 className="font-semibold text-6xl">Akhil's go shortlinks</h1>
+      <h1 className="font-semibold text-6xl">Akhil&#39;s go shortlinks</h1>
       <LinkForm />
     </main>
   );
